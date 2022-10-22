4b1b0b20a68fb0b6d8823f999882071472e32fc6
zhangcl <zhangcl@yunyoufreight.com>
2022-10-22 17:04:53 +0800

    快排修改

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -2,18 +2,24 @@ package sort;
 
 /**
  * 归并排序
+ * 1.如何拆
+ * 2.如何合
  */
 public class MergeSort {
 
     //static int[] arr = {2, 4, 6, 5, 3, 1};
 
     public static void main(String[] args) {
-        mergeArr();
+        int[] a = {2, 4, 6, 5, 3, 1, 9};
+        int[] arr = recursion(a);
+        printArr(arr);
+        //int[] b = {3, 5, 5, 6};
+        //mergeArr(a, b);
     }
 
-    public static void mergeArr() {
-        int[] a = {1, 2, 4, 5};
-        int[] b = {3, 5, 5, 6};
+    public static int[] mergeArr(int[] a, int[] b) {
+        //int[] a = {1, 2, 4, 5};
+        //int[] b = {3, 5, 5, 6};
         int[] c = new int[a.length + b.length];
 
         int index = 0;
@@ -50,7 +56,9 @@ public class MergeSort {
             j++;
         }
 
-        printArr(c);
+        //printArr(c);
+
+        return c;
     }
 
     public static void printArr(int[] arr) {
@@ -65,5 +73,37 @@ public class MergeSort {
         sb.append("]");
         System.out.println(sb.toString());
     }
+
+    public static int[] recursion(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int length = arr.length;
+        int middle = length / 2;
+        int[] leftArr = new int[middle];
+        int[] rightArr = new int[length - middle];
+
+        for (int i = 0; i < middle; i++) {
+            leftArr[i] = arr[i];
+        }
+        for (int i = middle; i < length; i++) {
+            rightArr[i - middle] = arr[i];
+        }
+
+        int[] leftSortedArr = recursion(leftArr);
+        int[] rightSortedArr = recursion(rightArr);
+
+        int[] mergedArr = mergeArr(leftSortedArr, rightSortedArr);
+        return mergedArr;
+
+        //System.out.println("leftArr = ");
+        //printArr(leftArr);
+
+        //System.out.println("rightArr = ");
+        //printArr(rightArr);
+    }
+
+
 
 }
--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,12 +1,68 @@
 package sort;
 
+import com.sun.istack.internal.NotNull;
+
+import java.util.Arrays;
+
 /**
  * 快速排序
+ * pivot的选择，将数组分为2部分
+ * 较小部分的尾，较大部分的头
+ * "状态"的思考方式, 既然是分类，那么边界点在哪？
+ * 什么是不变的，什么是变化的？
+ *
  */
 public class QuickSort {
 
-    static int[] arr = {2, 4, 6, 5, 3, 1};
+    static int[] arr = {2, 1, 6, 8, 5, 3, 4};
+    static int[] arr1 = {5, 7, 6};
+    static int[] arr2 = {7, 5, 6};
+    static int[] arr3 = {7, 5};
+    static int[] arr4 = {5, 6};
 
+    public static void main(String[] args) {
+        rearrange(arr);
+        System.out.println(Arrays.toString(arr));
+        rearrange(arr1);
+        System.out.println(Arrays.toString(arr1));
+        rearrange(arr2);
+        System.out.println(Arrays.toString(arr2));
+        rearrange(arr3);
+        System.out.println(Arrays.toString(arr3));
+        rearrange(arr4);
+        System.out.println(Arrays.toString(arr4));
+    }
 
+    public static int rearrange(int[] arr) {
+        if (arr.length == 1) {
+            return 0;
+        }
+        int length = arr.length;
+        int pivot = arr[length - 1];
+        int highIdx = -1;
+
+        for (int i = 0; i <= length - 2; i++) {
+            if (arr[i] > pivot && highIdx == -1) {
+                highIdx = i;
+            }
+            if (arr[i] < pivot && highIdx != -1) {
+                int temp = arr[i];
+                arr[i] = arr[highIdx];
+                arr[highIdx] = temp;
+                highIdx++;
+            }
+        }
+
+        if (highIdx > -1) {
+            arr[length - 1] = arr[highIdx];
+            arr[highIdx] = pivot;
+        }
+
+        return highIdx;
+    }
+
+    public static void quickSort(int[] arr) {
+        //rearrange(arr);
+    }
 
 }
