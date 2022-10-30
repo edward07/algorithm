2559df619cad0021d8bffef57d908db8fb982c6e
zhangcl <zhangcl@yunyoufreight.com>
2022-10-30 17:39:01 +0800

    最大窗口deque

--- src/queue/MaxSlidingWindow.java
+++ src/queue/MaxSlidingWindow.java
@@ -2,14 +2,11 @@ package queue;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Deque;
 import java.util.PriorityQueue;
 
 public class MaxSlidingWindow {
 
-    private PriorityQueue<Integer> pq;
-
-    private Deque<Integer> deque;
+    private PriorityQueue<int[]> pq;
 
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums.length  == 0 || nums.length < k) {
@@ -18,45 +15,36 @@ public class MaxSlidingWindow {
         int length = nums.length;
         int[] arr = new int[length - k + 1];
 
-        pq = new PriorityQueue<>(k, new Comparator<Integer>() {
+        pq = new PriorityQueue<>(k, new Comparator<int[]>() {
             @Override
-            public int compare(Integer o1, Integer o2) {
-                if (o1 > o2) {
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] > o2[0]) {
                     return -1;
-                } else if (o1 < o2) {
+                } else if (o1[0] < o2[0]) {
                     return 1;
                 }
                 return 0;
             }
         });
 
-        for (int i = 0; i < k; i++) {
-            pq.add(nums[i]);
-        }
+        int start = 0;
+        int end = 0;
 
-        for (int j = k; j < length; j++) {
-            int prev = nums[j-k];
-            int currMax = pq.peek();
-            int next = nums[j];
-
-            if (prev == currMax) {
-                arr[j-k] = currMax;
-                pq.poll();
-                pq.add(next);
-            } else {
-                if (next >= currMax) {
+        for (int i = 0; i < length; i++) {
+            pq.add(new int[]{nums[i], i});
+            if (i >= k) {
+                start++;
+            }
+            end++;
+            if (i >= k - 1) {
+                int[] peek = pq.peek();
+                while (peek[1] < start) {
                     pq.poll();
-                    pq.add(next);
-                    arr[j-k] = next;
-                } else {
-                    arr[j-k] = currMax;
-                    pq.poll();
-                    pq.add(next);
+                    peek = pq.peek();
                 }
+                arr[i-(k-1)] = peek[0];
             }
         }
-
-        arr[length - k] = pq.peek();
 
         return arr;
     }
